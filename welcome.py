3c1b6fc845a9b9caa59b6bda646caa42439406a0
NewbiewhoCodes <64906855+NewbiewhoCodes@users.noreply.github.com>
2020-05-07 20:28:13 +0530

    "Welcome to Newbie Codes"
    Basic Python Program for Print Statement

---
+++ welcome.py
@@ -0,0 +1,7 @@
+#Welcome to My First Code .
+
+print("Welcome to Newbie Codes")
+
+#Print Statement is used to Print the value provided in "( )".
+
+#We can Print any String or Variable using Print statement.In the above programe you can change "Welcome to Newbie Codes" with " Any String You Want To Print On Console ".
